23673aa1c4e40b1df9bbdd7c20867f836f5c853c
Ian Lyttle <ian.lyttle@schneider-electric.com>
2018-04-08 14:31:32 -0500

    adds tests for tooltip-instantiation, tweaks NEWS

--- tests/testthat/test-vega-tooltip.R
+++ tests/testthat/test-vega-tooltip.R
@@ -1,5 +1,30 @@
 context("vega-tooltip")
 
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+tooltip_default <- list(
+  showAllFields = FALSE,
+  delay = 100,
+  colorTheme = "light",
+  sort = "title"
+)
+
+# a mutate() for lists would be nice here
+tooltip_all <- tooltip_default
+tooltip_all$showAllFields = TRUE
+
+tooltip_encoding <- tooltip_default
+tooltip_encoding$showAllFields = NULL
+
+test_that("instantiation works", {
+
+  # vega_tooltip()
+  tooltip_test <- vega_tooltip()
+  expect_equivalent(tooltip_test, tooltip_default)
+  expect_is(tooltip_test, "vega_tooltip")
+
+  # vega_tooltip_encoding()
+  expect_equivalent(vega_tooltip_encoding(), tooltip_encoding)
+
+  # vega_tooltip_all()
+  expect_equivalent(vega_tooltip_all(), tooltip_all)
+
 })
