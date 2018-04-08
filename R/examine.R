12318fcb51d6d85e6ecaead099f2710c6ab1ea08
Ian Lyttle <ian.lyttle@schneider-electric.com>
2018-04-08 12:22:00 -0500

    introduces some S3 argle-bargle for examine()

--- R/examine.R
+++ R/examine.R
@@ -5,11 +5,42 @@
 #' @inheritParams listviewer::jsonedit
 #'
 #' @return called for side-effects
+#'
 #' @export
 #'
 examine <- function(listdata = NULL, mode = "tree",
                     modes = c("code", "form", "text", "tree", "view"), ...,
                     width = NULL, height = NULL, elementId = NULL) {
+  UseMethod("examine")
+}
+
+#' @export
+#'
+examine.default <- function(listdata = NULL, mode = "tree",
+                            modes = c("code", "form", "text", "tree", "view"), ...,
+                            width = NULL, height = NULL, elementId = NULL) {
+  .examine(
+    listdata = listdata,
+    mode = mode,
+    modes = modes,
+    ...,
+    width = width,
+    height = height,
+    elementId = elementId
+  )
+}
+
+examine.vega_tooltip <- function(listdata = NULL, mode = "tree",
+                                 modes = c("code", "form", "text", "tree", "view"), ...,
+                                 width = NULL, height = NULL, elementId = NULL) {
+  NextMethod()
+}
+
+
+# internal function
+.examine <- function(listdata = NULL, mode = "tree",
+                     modes = c("code", "form", "text", "tree", "view"), ...,
+                     width = NULL, height = NULL, elementId = NULL) {
 
   if (!requireNamespace("listviewer", quietly = TRUE)) {
     stop("Package \"listviewer\" needed for this function to work. Please install it.",
