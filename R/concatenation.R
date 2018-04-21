1db8bf4be981ee2cad7edd6685be48b16270a93e
AliciaSchep <aschep@gmail.com>
2018-04-21 14:12:58 -0700

    updates for nicer pkgdown

--- R/concatenation.R
+++ R/concatenation.R
@@ -13,6 +13,7 @@
 #' @name altair_concatenation
 #' @examples
 #'
+#' # Examples using the beaver1 and beaver2 body temperature data sets
 #' # Layering Charts
 #' base = alt$Chart(r_to_py(beaver1))$encode(
 #'   x = alt$X('time'),
@@ -22,7 +23,7 @@
 #' scatter_plot <- base$mark_point()
 #' line_plot <- base$mark_line()
 #'
-#' vegalite(scatter_plot + line_plot)
+#' combined_plot <- scatter_plot + line_plot
 #'
 #' # Horizontal Concatenation
 #' base2 = alt$Chart(r_to_py(beaver2))$encode(
@@ -36,11 +37,13 @@
 #' combined_plot <- (scatter_plot + line_plot)$properties(title = "Beaver 1", width = 200)
 #' combined_plot2 <- (scatter_plot2 + line_plot2)$properties(title = "Beaver 2", width = 200)
 #'
-#' vegalite(combined_plot | combined_plot2, height = 400)
+#' hconcat_plot <- combined_plot | combined_plot2
 #'
 #' # Vertical Concatenation
 #'
-#' vegalite(combined_plot & combined_plot2, height = 600)
+#' vconcat_plot <- combined_plot & combined_plot2
+#'
+#' # Use vegalite function to render combined_plot, hconcat_plot, or vconcat_plot
 NULL
 
 is_altair_chart <- function(x) {
