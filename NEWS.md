4ab63039f91a0b24b73b18336c7c577b8885c617
Ian Lyttle <ian.lyttle@schneider-electric.com>
2018-04-22 21:43:35 -0500

    adds `vega_data`

--- NEWS.md
+++ NEWS.md
@@ -1,5 +1,7 @@
 # altair 0.1.9000
 
+* makes Vega datasets available as `vega_data` (#25)
+
 * updates css for embedding, makes the action-links look much better (#20, @aliciaschep)
 
 * adds tooltip-capability to `vegalite()` renderer, with functions `vega_tooltip()`, `add_fields()`, and friends: `vega_tooltip_encoding()` and `vega_tooltip_all()`. (#4) 
--- R/zzz.R
+++ R/zzz.R
@@ -15,17 +15,47 @@
 #' @examples
 #'   plot_basic <-
 #'     alt$Chart(
-#'       r_to_py(mtcars)
+#'       r_to_py(vega_data$cars())
 #'     )$encode(
-#'       x = "mpg:Q",
-#'       y = "hp:Q",
-#'       color = "cyl:N"
+#'       x = "Miles_per_Gallon:Q",
+#'       y = "Horsepower:Q",
+#'       color = "Origin:N"
 #'     )$mark_point()
 #'
+#' @seealso [Altair Python documentation](https://altair-viz.github.io/index.html)
 #' @export alt
 #'
 alt <- NULL
 
+#' Vega datasets
+#'
+#' Uses the reticulate framework to access the `vega_datasets` Python
+#' package.
+#'
+#' Some data are available locally, others require an internet connection.
+#'
+#' @param ... not used
+#'
+#' @examples
+#' # To list available datasets
+#' vega_data$list_datasets()
+#'
+#' # When accessing a dataset, substitute any "-" in the name with a "_"
+#' head(vega_data$sf_temps())
+#'
+#' # Metadata are available for each dataset:
+#' vega_data$sf_temps$description
+#' vega_data$sf_temps$url
+#'
+#' # For local datasets, local path is available
+#' vega_data$sf_temps$filepath
+#'
+#' @seealso [Vega datasets documentation](https://github.com/altair-viz/vega_datasets)
+#' @export vega_data
+#'
+vega_data <- NULL
+
 .onLoad <- function(libname, pkgname) {
   alt <<- reticulate::import("altair", delay_load = TRUE)
+  vega_data <<- reticulate::import("vega_datasets", delay_load = TRUE)$data
 }
