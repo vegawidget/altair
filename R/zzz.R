310b2b84ac10775288b3d5a35d436b02df66be89
Ian Lyttle <ian.lyttle@schneider-electric.com>
2018-05-01 15:56:07 -0500

    adds function import_vega_data()

--- R/zzz.R
+++ R/zzz.R
@@ -29,39 +29,6 @@
 #'
 alt <- NULL
 
-#' Vega datasets
-#'
-#' Uses the reticulate framework to access the `vega_datasets` Python
-#' package.
-#'
-#' Some data are available locally, others require an internet connection.
-#'
-# @param ... not used
-#'
-#' @examples
-#' # To list available datasets
-#' vega_data$data$list_datasets()
-#'
-#' # When accessing a dataset, substitute any "-" in the name with a "_"
-#' head(vega_data$data$sf_temps())
-#'
-#' # Metadata are available for each dataset:
-#' vega_data$data$sf_temps$description
-#' vega_data$data$sf_temps$url
-#'
-#' # For local datasets, local path is available
-#' vega_data$data$sf_temps$filepath
-#'
-#' @seealso [Vega datasets documentation](https://github.com/altair-viz/vega_datasets)
-#' @export vega_data
-#'
-vega_data <- NULL
-#scipy <- NULL
-#stats <- NULL
-
 .onLoad <- function(libname, pkgname) {
   alt <<- reticulate::import("altair", delay_load = TRUE)
-  vega_data <<- reticulate::import("vega_datasets", delay_load = TRUE)
-#  scipy <<- reticulate::import("scipy", delay_load = TRUE)
-#  stats <<- reticulate::import("scipy.stats", delay_load = TRUE)
 }
