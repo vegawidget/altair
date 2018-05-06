2355ea746988059169c03854e63800c6f6804190
Ian Lyttle <ian.lyttle@schneider-electric.com>
2018-05-06 11:26:12 -0500

    fixes #41: updates install_altair(), adds option to indicate which pyhton version is supported

--- R/install.R
+++ R/install.R
@@ -1,7 +1,8 @@
 #' Install Altair Python package
 #'
 #' This function wraps [reticulate::py_install()] to provide a straightforward,
-#' consistent method to install the Altair Python package.
+#' consistent method to install the Python packages
+#' **altair** and **vega_datasets**.
 #'
 #' This package uses the [reticulate][reticulate::reticulate] package
 #' to make an interface with the [Altair](https://altair-viz.github.io/index.html)
@@ -10,19 +11,20 @@
 #' [recommended](https://rstudio.github.io/reticulate/articles/package.html#installing-python-dependencies)
 #' to use a common Python environment, called `"r-reticulate"`.
 #'
-#' If you have a Conda installation, you can create this environment using
-#' [reticulate::conda_install()], as described in this
+#' Depending on your setup, you can create this environment using
+#' [reticulate::conda_create()] or [reticulate::virtualenv_install()],
+#' as described in this
 #' [reticulate article](https://rstudio.github.io/reticulate/articles/python_packages.html#conda-installation),
 #' or in this package's [Field-Guide to Python Issues](https://ijlyttle.github.io/altair/articles/field-guide-python.html#python-env).
 #'
-#' At present, version 2 of the Altair Python package is available as a
-#' release candidtate on PyPi, so it can be installed using `pip`.
-#' This function will use `pip` to install `altair==2.0.0rc2`
-#' into your `"r-reticulate"` Python environment.
-#'
-#' @param pip TRUE to use pip (defaults to TRUE)
-#' @inheritParams reticulate::py_install
-#' @inheritParams reticulate::conda_install
+#' @param method `character`, indicates to use `"conda"` or `"virtualenv"`
+#' @param envname `character`, name of environment into which to install
+#' @param pip, `logical`, used for conda installation to indicate to use pip
+#'   (will be set to TRUE for release-candidates)
+#' @param version `character`, version of Altair to install. For general use of this package,
+#'   this is set automatically, so you should not need to specify this.
+#' @param ... other arguments sent to [reticulate::conda_install()] or
+#'    [reticulate::virtualenv_install()]
 #'
 #' @return invisible `NULL`, called for side-effects
 #'
@@ -35,17 +37,53 @@
 #' }
 #' @export
 #'
-install_altair <- function(method = "auto", conda = "auto", pip = TRUE, ...) {
+install_altair <- function(method = c("conda", "virtualenv"),
+                           envname = "r-reticulate",
+                           pip = FALSE,
+                           version = getOption("altair.python.version"),
+                           ...) {
 
-  # TODO: check version of reticulate
+  # validate stage, method arguments
+  method <- match.arg(method)
 
-  reticulate::py_install(
-    "altair==2.0.0rc2",
-    method = method,
-    conda = conda,
-    pip = pip,
-    ...
-  )
+  # determine if this is a release candidate
+  is_release_candidate <- grepl(version, "rc")
+
+  if (identical(method, "conda") && is_release_candidate) {
+    pip <- TRUE
+    message("Package not available on conda-forge, setting pip to TRUE")
+  }
+
+  # conda and pip use different syntax for indicating versions
+  if (identical(method, "conda") && !pip) {
+    version_sep <- "="
+  } else {
+    version_sep <- "=="
+  }
+
+  altair_pkg_version <- paste("altair", version, sep = version_sep)
+
+  packages <- c(altair_pkg_version, "vega_datasets")
+
+  # call installer
+  if (identical(method, "conda")) {
+    reticulate::conda_install(
+      packages = packages,
+      envname = envname,
+      pip = pip,
+      ...
+    )
+  }
+
+  if (identical(method, "virtual")) {
+    reticulate::virtualenv_install(
+      packages = packages,
+      envname = envname,
+      ...
+    )
+  }
 
   invisible(NULL)
 }
+
+
