5b98cbf410bfb121a6e4eb2d07ecd2bfc190ef45
Ian Lyttle <ian.lyttle@schneider-electric.com>
2018-05-06 21:29:18 -0500

    removes tooltip functions, fixes #45

--- inst/htmlwidgets/vegalite.js
+++ inst/htmlwidgets/vegalite.js
@@ -11,12 +11,9 @@ HTMLWidgets.widget({
       renderValue: function(x) {
 
         var chart_spec = JSON.parse(x.chart_spec);
-        var tooltip_options = x.tooltip_options;
         var embed_options = x.embed_options;
 
         vegaEmbed(el, chart_spec, opt = embed_options).then(function(result) {
-          // access view as result.view
-          vegaTooltip.vegaLite(result.view, chart_spec, tooltip_options);
 
           // By removing the style (width and height) of the
           // enclosing element, we let the "chart" decide the space it
