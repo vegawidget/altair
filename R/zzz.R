4a31ba21d1aee76942c2bc2256e05c4224b9cdde
AliciaSchep <aschep@gmail.com>
2018-06-06 21:27:42 -0700

    update to 2.1.0

--- R/zzz.R
+++ R/zzz.R
@@ -71,7 +71,7 @@ on_altair_error <- function(e) {
 .onLoad <- function(libname, pkgname) {
 
   # sets the supported version
-  options(altair.python.version = "2.0.1")
+  options(altair.python.version = "2.1.0")
 
   alt <<-
     reticulate::import(
