1f4120436f371e56c405a2d9b0bb9d896f65f140
Ian Lyttle <ian.lyttle@schneider-electric.com>
2019-06-04 15:38:26 -0500

    Move to Altair 3.0.0

--- R/zzz.R
+++ R/zzz.R
@@ -57,7 +57,7 @@ on_altair_error <- function(e) {
 .onLoad <- function(libname, pkgname) {
 
   # sets the supported version
-  options(altair.python.version = "2.3.0")
+  options(altair.python.version = "3.0.0")
 
   alt <<-
     reticulate::import(
